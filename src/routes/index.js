5fad6294b150d88827146a458a626677d2fde122
Lucas Braga <lucas23an@hotmail.com>
2020-01-21 01:34:21 -0300

    Aula Utilizando Root Import

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,21 @@
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '../pages/Signin';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
